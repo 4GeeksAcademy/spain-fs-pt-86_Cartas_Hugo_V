fa9c4157bd95e18aa31ee359bd865ddd8c6ccd66
Hvaxquez <hugoferney54@gmail.com>
2024-12-03 13:08:11 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["♠", "♥", "♦", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  function generateCard() {
+    const suit = suits[Math.floor(Math.random() * suits.length)];
+    const value = values[Math.floor(Math.random() * values.length)];
+
+    document.getElementById("top-left").textContent = `${value} ${suit}`;
+    document.getElementById("center").textContent = suit;
+    document.getElementById("bottom-right").textContent = `${value} ${suit}`;
+  }
+
+  console.log("generateCard");
 };
