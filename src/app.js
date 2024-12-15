2198fb829f041a1c89b367ba6f5082d671374c06
Hvaxquez <hugoferney54@gmail.com>
2024-12-15 19:11:28 +0000

    final carta

--- src/app.js
+++ src/app.js
@@ -5,32 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  const suits = ["♠", "♥", "♦", "♣"];
-  const values = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
+const suits = [
+  { color: "black", icon: "♠" },
+  { color: "red", icon: "♥" },
+  { color: "red", icon: "♦" },
+  { color: "black", icon: "♣" }
+];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
 
-  function generateCard() {
-    const suit = suits[Math.floor(Math.random() * suits.length)];
-    const value = values[Math.floor(Math.random() * values.length)];
-
-    document.getElementById("top-left").textContent = `${value} ${suit}`;
-    document.getElementById("center").textContent = suit;
-    document.getElementById("bottom-right").textContent = `${value} ${suit}`;
-  }
-
-  console.log("generateCard");
-};
+function generateCard() {
+  const suit = suits[Math.floor(Math.random() * suits.length)];
+  const value = values[Math.floor(Math.random() * values.length)];
+  const topleft = document.getElementById("top-left");
+  const center = document.getElementById("center");
+  const bottonright = document.getElementById("bottom-right");
+  topleft.textContent = `${value} ${suit.icon}`;
+  center.textContent = suit.icon;
+  bottonright.textContent = `${value} ${suit.icon}`;
+  topleft.classList.remove("red", "black");
+  center.classList.remove("red", "black");
+  bottonright.classList.remove("red", "black");
+  topleft.classList.add(suit.color);
+  center.classList.add(suit.color);
+  bottonright.classList.add(suit.color);
+}
+window.generateCard = generateCard;
