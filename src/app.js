cc6f3fd19563f84f7ffef9d9a4be5caaeb5bf46a
Hvaxquez <hugoferney54@gmail.com>
2025-02-04 11:15:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+/* Estas líneas importan las dependencias necesarias, 
+incluyendo Bootstrap para estilos, un archivo CSS personalizado y algunas imágenes. */
+import "bootstrap"; 
+import "./style.css"; 
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const suits = [
-  { color: "black", icon: "♠" },
+/* Aquí se definen dos arreglos: 
+suits que contiene los palos de las cartas con su color e ícono,
+ y values que contiene los valores de las cartas. */
+
+const suits = [    
+  { color: "black", icon: "♠" },  
   { color: "red", icon: "♥" },
   { color: "red", icon: "♦" },
   { color: "black", icon: "♣" }
@@ -26,7 +32,13 @@ const values = [
   "Q",
   "K"
 ];
+// Esta función genera una carta aleatoria:
 
+//Selecciona un palo y un valor aleatorios.
+
+//Actualiza el contenido de los elementos HTML con los IDs top-left, center y bottom-right para mostrar el valor y el ícono del palo.
+
+//Ajusta las clases CSS de los elementos para aplicar el color correspondiente al palo.
 function generateCard() {
   const suit = suits[Math.floor(Math.random() * suits.length)];
   const value = values[Math.floor(Math.random() * values.length)];
@@ -43,4 +55,9 @@ function generateCard() {
   center.classList.add(suit.color);
   bottonright.classList.add(suit.color);
 }
+/* Esto permite que la función generateCard 
+sea accesible globalmente, 
+por lo que se puede llamar desde cualquier parte del código
+ o desde la consola del navegador. */
+
 window.generateCard = generateCard;
